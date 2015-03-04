8c67ba1e8db88c97bc12aa917f7fab7f4607f3bd
Sonja R. Maddox <[sonjarmaddox@yahoo.com]>
2015-03-04 09:12:14 -0600

    db2

--- home_splash/app/controllers/home_controller.rb
+++ home_splash/app/controllers/home_controller.rb
@@ -4,13 +4,13 @@ class HomeController <ApplicationController
     @current_time = right_now.to_formatted_s(:long)
     @greet_message = case right_now.hour 
       when 1..11
-       "Good Morning, Renee!" 
+       "Good Morning!" 
         
       when 12..15
-       "Good Afternoon, Renee!"
+       "Good Afternoon!"
           
       when 16..19
-       "Good Evening, Renee!"
+       "Good Evening!"
     
       when 20..00
        "Good Night!"
